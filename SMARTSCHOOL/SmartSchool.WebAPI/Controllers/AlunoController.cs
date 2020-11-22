1e083039c67de98c1ca673e577787b6c3ced3adb
AdrianoSantana <ans3@cin.ufpe.br>
2020-11-22 11:36:20 -0300

    Adicionado banco de dados com carga inicial

--- SMARTSCHOOL/SmartSchool.WebAPI/Controllers/AlunoController.cs
+++ SMARTSCHOOL/SmartSchool.WebAPI/Controllers/AlunoController.cs
@@ -1,48 +1,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using SmartSchool.WebAPI.Data;
 using SmartSchool.WebAPI.Models;
 
 namespace SmartSchool.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AlunoController: ControllerBase
+    public class AlunoController : ControllerBase
     {
-        public List<Aluno> Alunos = new List<Aluno> {
-            new Aluno() {
-                Id = 1,
-                Nome = "Adriano",
-                Sobrenome = "Santana",
-                Telefone = "81 9 9502 9086"
-            },
-            new Aluno() {
-                Id = 2,
-                Nome = "AAAAAAAAA",
-                Sobrenome = "Santana",
-                Telefone = "81 9 9502 9086"
-            },
-            new Aluno() {
-                Id = 3,
-                Nome = "Laura",
-                Sobrenome = "Santana",
-                Telefone = "81 9 9502 9086"
-            },
-        };
-        public AlunoController()
+        private readonly DataContext _context;
+        public AlunoController(DataContext context)
         {
-            
+            _context = context;
+
         }
 
         [HttpGet]
-        public IActionResult Get() {
-            return Ok(Alunos);
+        public IActionResult Get()
+        {
+            return Ok(_context.Alunos);
         }
 
         // /api/aluno/byId?Id=2
         [HttpGet("{id}")]
-        public IActionResult GetById(int id) {
-            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+        public IActionResult GetById(int id)
+        {
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Id == id);
             if (aluno == null) return BadRequest("Não existe um aluno com este Id");
             return Ok(aluno);
         }
@@ -50,27 +35,31 @@ namespace SmartSchool.WebAPI.Controllers
         // /api/aluno/nome
 
         [HttpGet("byName")]
-        public IActionResult GetByName(string nome, string sobrenome) {
-            var aluno = Alunos.FirstOrDefault(a => a.Nome.Contains(nome) && a.Sobrenome.Contains(sobrenome));
+        public IActionResult GetByName(string nome, string sobrenome)
+        {
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Nome.Contains(nome) && a.Sobrenome.Contains(sobrenome));
             if (aluno == null) return BadRequest("Não existe um aluno com este nome");
             return Ok(aluno);
         }
 
         [HttpPost]
 
-        public IActionResult Post(Aluno aluno) {
+        public IActionResult Post(Aluno aluno)
+        {
             return Ok(aluno);
         }
 
         [HttpPut("{id}")]
 
-        public IActionResult Put(int id, Aluno aluno) {
+        public IActionResult Put(int id, Aluno aluno)
+        {
             return Ok(aluno);
         }
 
-         [HttpDelete("{id}")]
+        [HttpDelete("{id}")]
 
-        public IActionResult Delete(int id) {
+        public IActionResult Delete(int id)
+        {
             return Ok();
         }
     }
--- SMARTSCHOOL/SmartSchool.WebAPI/Startup.cs
+++ SMARTSCHOOL/SmartSchool.WebAPI/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SmartSchool.WebAPI.Data;
 
 namespace SmartSchool.WebAPI
 {
@@ -25,7 +27,8 @@ namespace SmartSchool.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+         services.AddDbContext<DataContext>(options  => options.UseSqlite(Configuration.GetConnectionString("Default")));
+         services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
